6d9e33de0c8803019516075464f241eb89360970
Humberto Antonio Marquez <humberto@2gom.com.mx>
2017-12-07 12:19:59 -0600

    Botones para filtrado de citas

--- views/citas/index.php
+++ views/citas/index.php
@@ -27,22 +27,79 @@ $this->registerJsFile(
 ?>
 
 
-<?php Pjax::begin(['id' => 'citas', 'timeout'=>'0', 'linkSelector'=>'table thead a']) ?>
-<div class="panel-group" id="exampleAccordionDefault" aria-multiselectable="true" role="tablist">
-    <div class="panel">
-        <div class="panel-heading" id="exampleHeadingDefaultOne" role="tab">
-            <a class="panel-title <?=Yii::$app->request->get('isOpen')?'':'collapsed' ?> js-collapse" data-toggle="collapse" href="#exampleCollapseDefaultOne" data-parent="#exampleAccordionDefault" aria-expanded="<?=Yii::$app->request->get('isOpen')?'true':'false' ?>" aria-controls="exampleCollapseDefaultOne">
-                Buscar cita
+<?php Pjax::begin(['id' => 'citas', 'timeout'=>'0', 'linkSelector'=>'table thead a, a.list-group-item']) ?>
+
+<div class="row">
+    <div class="col-md-3">
+        <div class="list-group bg-blue-grey-100">
+            <?php
+            foreach($status as $statu){
+                switch ($statu->id_statu_cita) {
+                    case '1':
+                        $statusColor = 'warning';
+                        break;
+                    case '2':
+                        $statusColor = ' bg-brown-800';
+                        break;
+                    case '3':
+                        $statusColor = ' bg-blue-800';
+                        break;    
+                    case '4':
+                        $statusColor = 'danger';
+                        break;
+                    case '5':
+                        $statusColor = 'danger';
+                    break;  
+                    case '6':
+                        $statusColor = ' bg-blue-800';
+                    break;  
+                    case '7':
+                        $statusColor = 'success';
+                    break;   
+                    case '8':
+                        $statusColor = 'success';
+                    break;      
+                    default:
+                        # code...
+                        break;
+                }
+            ?>
+
+            <a class="list-group-item blue-grey-500" href="<?=Url::base()?>/citas/index?EntCitasSearch[id_status]=<?=$statu->id_statu_cita?>">
+                <i class="icon wb-calendar" aria-hidden="true"></i>  
+                <span class="badge badge-<?=$statusColor?> text-white">
+                    <?=count($statu->entCitas)?>
+                </span>
+                <?=$statu->txt_nombre?>
             </a>
+            
+            <?php
+                }
+            ?>
+            <a class="list-group-item blue-grey-500" href="<?=Url::base()?>/citas/index">
+                Mostrar todas
+            </a>
+           
         </div>
-        <div class="panel-collapse collapse <?=Yii::$app->request->get('isOpen')?'in':'' ?>" id="exampleCollapseDefaultOne" aria-labelledby="exampleHeadingDefaultOne" role="tabpanel" aria-expanded="<?=Yii::$app->request->get('isOpen')?'true':'false' ?>">
-            <div class="panel-body">
-                <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+    </div>
+    <div class="col-md-9">
+        <div class="panel-group" id="exampleAccordionDefault" aria-multiselectable="true" role="tablist">
+            <div class="panel">
+                <div class="panel-heading" id="exampleHeadingDefaultOne" role="tab">
+                    <a class="panel-title  js-collapse" data-toggle="collapse" href="#exampleCollapseDefaultOne" data-parent="#exampleAccordionDefault" aria-expanded="true" aria-controls="exampleCollapseDefaultOne">
+                        Buscar cita
+                    </a>
+                </div>
+                <div class="panel-collapse collapse in" id="exampleCollapseDefaultOne" aria-labelledby="exampleHeadingDefaultOne" role="tabpanel" aria-expanded="true">
+                    <div class="panel-body">
+                        <?php echo $this->render('_search', ['model' => $searchModel]); ?>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    
 </div>
-
 
  <!-- Panel -->
  <div class="panel" id="panel">
